829eb3fbc1ee595dbeec988119e72948265ba4fa
Stephan Dowless <dowless.stephan@gmail.com>
2021-12-25 17:57:57 -0500

    Initial Commit

---
+++ SwiftUIUberEatsMenu/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUIUberEatsMenu
+//
+//  Created by Stephen Dowless on 12/25/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUIUberEatsMenu/SwiftUIUberEatsMenuApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIUberEatsMenuApp.swift
+//  SwiftUIUberEatsMenu
+//
+//  Created by Stephen Dowless on 12/25/21.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIUberEatsMenuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
